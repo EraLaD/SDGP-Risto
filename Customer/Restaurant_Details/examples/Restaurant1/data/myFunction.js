29a8dd34820752d9fbb54434e16e8e0873b0f83b
Erandi Dayasena <laserandi@gmail.com>
2016-05-17 23:55:41 +0530

    Add files via upload

---
+++ Customer/Restaurant_Details/examples/Restaurant1/data/myFunction.js
@@ -0,0 +1,27 @@
+myFunction([
+{
+    "id":"1",
+	"name":"The Lagoon",
+	"province":"Western",
+    "location":"77, Galle Road,Colombo 03",
+	"nearest_town":["colombo 01", "colombo 02", "colombo 03", "colombo 04", "colombo 05", "colombo 06"],
+    "Rating": "5",
+    "min_price":1200,
+	"max_price":4500,
+	"restaurant_open_time":"08.00",
+	"restaurant_close_time":"00.00",
+	"services":["Buffet", "Delivery", "Free Wifi", "Parking Area", "Online Order"],
+	"lunch_open_time":"12.00",
+	"lunch_close_time":"15.00",
+	"dinner_open_time":"19.00",
+	"startDate":"2016-01-01",
+	"endDate":"2016-12-31",
+	"short_description":"There are plenty of seafood restaurants in Colombo, but at Lagoon add a little extra to the experience. The restaurant has been designed for happy chaos and lively banter, similar to the hubbub of a seafood market – with our abundant, 5-star atmosphere.",
+	"picture":"images/restaurants/restaurant1.jpg",
+	"alt": "restaurant1",
+	"long_description":"Lagoon offere the best & the freshest seafood in town because we get the freshest catch comes in the early morning hours, ready to be turned into a food experience you never had before. If you come for lunch or dinner and you’ll see how we keep the pace of the morning market alive throughout the day with our popular, crowded rooms, constantly buzzing with action.We provide exceptional service on all fronts. All the waiters were polite and knowledgeable, with at least two of them always around to attend to tables. Even when it came to selecting seafood, there was one waiter to advise us on how to pair our seafood with the preparations.",
+	"url":"Restaurant1/restaurant1Home.php"
+  }
+    
+
+]);
